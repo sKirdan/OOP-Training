76fe94d41d139fa7b0ba01917338200fb4c14c4c
User <rex1532007@gmail.com>
2019-09-01 20:22:55 +0300

    created ui interface

--- L1/Pirate/Main.java
+++ L1/Pirate/Main.java
@@ -1,4 +1,22 @@
 package L1.Pirate;
 
 public class Main {
+    public static void main(String[] args) {
+        Bar bar = new Bar();
+        Buhta buhta = new Buhta();
+
+        Ship ship = buhta.getShip();
+        Pirate cap = bar.gatCap();
+        Pirate[] pirates = bar.getPirates();
+
+        ship.setCap(cap);
+        ship.setPirates(pirates);
+
+        ship.info();
+
+        Scena_1.go(ship);
+
+        ship.info();
+
+    }
 }
